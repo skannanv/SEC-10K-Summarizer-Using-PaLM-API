7366128f71f93f29f181d1c989c2d895b139360c
vidhyakshayakannan <vidhyakshaya@gmail.com>
2024-05-07 13:54:55 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import re
 import google.generativeai as palm
 from sec_api import ExtractorApi  # Import SEC Extractor API
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from textblob import TextBlob
 import config
 
 # Function to clean text
@@ -24,8 +22,8 @@ def summarize_text_chunks(text_chunks, model):
     summaries = []
     for i, chunk in enumerate(text_chunks):
         prompt = "I collected some SEC filing data and want to summarize it. " \
-                 "Please summarize and output 3 BULLET POINTS ONLY. Do not use the words 'bullet points' " \
-                 "in your answer. Make sure the output is WELL FORMATTED and EASY TO UNDERSTAND. Otherwise, a child will die. If there is no relevant or important information, output an empty string. The relevant information is below:\n" + chunk
+                 "Please summarize and output 10 BULLET POINTS ONLY. Do not use the words 'bullet points' " \
+                 "in your answer. Make sure the output is WELL FORMATTED and EASY TO UNDERSTAND. Otherwise, a child will die. The relevant information is below:\n" + chunk
         print("Generating summary for Chunk", i + 1)
         completion = palm.generate_text(
             model=model,
@@ -37,14 +35,9 @@ def summarize_text_chunks(text_chunks, model):
         summaries.append(summary)  # Append the summary to the list of summaries
     return summaries
 
-# Function to perform sentiment analysis
-def analyze_sentiment(text):
-    blob = TextBlob(text)
-    sentiment = blob.sentiment
-    return sentiment
 
 # Initialize SEC Extractor API
-extractorApi = ExtractorApi("YOUR_API_KEY")
+extractorApi = ExtractorApi("9458b344ac5df97a8178aeb1751e759b5ce49528e35de2b365f7c813ce4dc23c")
 palm.configure(api_key=config.API_KEY)
 # Streamlit UI
 st.title("SEC Filing Summarization App")
@@ -53,9 +46,11 @@ st.title("SEC Filing Summarization App")
 filing_url = st.text_input("Enter the SEC filing URL:")
 
 # Dropdown to select section
-sections = ['1', '1A', '7', '7A', '8', '9']
-selected_section = st.selectbox("Select the section of the SEC filing to summarize:", sections)
-
+sections = ['1', '1A', '1B', '6', '7', '7A', '8', '9']
+sections_dict = {"1": "Business", "1A": "Risk Factors", "1B" : "Unresolved Staff Comments", "6": "Selected Financial Data", "7": "Management Discussion and Analysis of Financial Condition and Results of Operation", "7A": "Quantitative and Qualitative Disclosures about Market Risk", "8": "Financial Statements and Supplementary Data", "9": "Changes in and Disagreements with Accountants on Accounting and Financial Disclosure"}
+selected = st.selectbox("Select the section of the SEC filing to summarize:", sections_dict.items())
+selected_pair = selected[0]  # Accessing the first key-value pair
+selected_section = selected_pair[0] 
 if st.button("Summarize"):
     if filing_url:
         # Extract text from SEC filing using the Extractor API
@@ -76,13 +71,9 @@ if st.button("Summarize"):
 
         # Combine summaries into a single text
         # Flatten the list of lists
-        combined_summary = " ".join(point for sublist in summaries for point in sublist if point is not None)
+        combined_summary = " ".join(str(point) for sublist in summaries for point in sublist if point is not None)
 
-        # Perform sentiment analysis
-        sentiment = analyze_sentiment(combined_summary)
-        st.subheader("Sentiment Analysis")
-        st.write(f"Overall Sentiment: {sentiment.polarity}")
-        
+      
         # Display the summaries
         st.subheader("Summary")
         for i, summary in enumerate(summaries):
